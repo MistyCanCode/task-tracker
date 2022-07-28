657f9dcf26b5ba0dc147779b115c3b1a55a4cdc5
Misty Tomlin (OSV) <misty.tomlin.osv@fedex.com>
2022-07-28 14:08:27 -0500

    Added button, header, task, and task-item components

---
+++ src/app/mock-tasks.ts
@@ -0,0 +1,34 @@
+import {Task} from './Task'
+
+export const TASKS: Task[] = [
+    {
+        id: 1,
+        text: 'Endoscopy Appointment',
+        day: 'July 22nd at 8:00am',
+        reminder: true,
+    },
+    {
+        id: 2,
+        text: 'Vet Appointment',
+        day: 'July 26th at 10:00am',
+        reminder: true,
+    },
+    {
+        id: 3,
+        text: 'Dog grooming Appointment',
+        day: 'July 28th at 9:00am',
+        reminder: true,
+    },
+    {
+        id: 4,
+        text: 'Daily Treadmill Time',
+        day: 'July 29th at 7:00am',
+        reminder: true,
+    },
+    {
+        id: 5,
+        text: 'Daily Stand Up Meeting',
+        day: 'July 29th at 9:00am',
+        reminder: false,
+    }
+]
